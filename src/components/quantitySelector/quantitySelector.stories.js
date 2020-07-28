61438847b435b5c21c45f04f5224157151920fd4
mschmauder-hub <marius.schmauder.ms@gmail.com>
2020-07-28 11:30:04 +0200

    Add quantitySelector dynamic

--- src/components/quantitySelector/quantitySelector.stories.js
+++ src/components/quantitySelector/quantitySelector.stories.js
@@ -1,6 +1,30 @@
+import { useEffect } from "@storybook/client-api";
 import "./quantitySelector.css";
 import quantitySelector from "./quantitySelector.html";
 
 export default { title: "quantitySelector" };
 
-export const simpleSelector = () => quantitySelector;
+export const simpleSelector = () => {
+  useEffect(() => {
+    const decrease = document.querySelector(".decrease-btn");
+    const increase = document.querySelector(".increase-btn");
+    const resultElement = document.querySelector(".counter");
+
+    decrease.addEventListener("click", () => {
+      const oldResult = Number(resultElement.innerHTML);
+      if (oldResult === 2) {
+        decrease.disabled = true;
+      }
+      resultElement.innerHTML = oldResult - 1;
+    });
+
+    increase.addEventListener("click", () => {
+      const oldResult = Number(resultElement.innerHTML);
+      if (oldResult === 1) {
+        decrease.disabled = false;
+      }
+      resultElement.innerHTML = oldResult + 1;
+    });
+  });
+  return quantitySelector;
+};
---
+++ src/pages/details/details.stories.js
@@ -0,0 +1,19 @@
+import { useEffect } from "@storybook/client-api";
+
+export default {
+  title: "Pages/Details",
+};
+export const basic = () => {
+  useEffect(() => {
+    const button = document.querySelector(".btn");
+    button.addEventListener("click", () => {
+      button.innerHTML = "Mehr Fische";
+    });
+  });
+  return '<button class="btn" >Hallo Fische</button>';
+};
+
+// const numberOfStudents = 15;
+// const text = `Hello  ${numberOfStudents} Fische`;
+
+// alert(text);
