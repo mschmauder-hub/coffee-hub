6b567a37fe60fb954a0a345f23e13a07c3cc5f15
mschmauder-hub <marius.schmauder.ms@gmail.com>
2020-07-29 12:23:33 +0200

    Refactor quantity selector code

---
+++ src/components/quantitySelector/quantitySelector.js
@@ -0,0 +1,48 @@
+import minus from "../../assets/minus.svg";
+import plus from "../../assets/plus.svg";
+import { createElement } from "../../utils/element";
+export function createQuantitySelector() {
+  const quantitySelector = createElement("div", {
+    className: "quantitySelector",
+  });
+
+  const decreaseButton = createElement("button", {
+    className: "quantity-btn decrease-btn",
+    disabled: true,
+  });
+  const increaseButton = createElement("button", {
+    className: "quantity-btn increase-btn",
+  });
+  const counter = createElement("div", {
+    className: "counter",
+    innerText: 1,
+  });
+
+  const minusImage = createElement("img", { src: minus, alt: "minus-icon" });
+  const plusImage = createElement("img", { src: plus, alt: "plus-icon" });
+
+  decreaseButton.append(minusImage);
+  increaseButton.append(plusImage);
+
+  quantitySelector.append(decreaseButton);
+  quantitySelector.append(counter);
+  quantitySelector.append(increaseButton);
+
+  decreaseButton.addEventListener("click", () => {
+    const oldResult = Number(counter.innerHTML);
+    if (oldResult === 2) {
+      decreaseButton.disabled = true;
+    }
+    counter.innerHTML = oldResult - 1;
+  });
+
+  increaseButton.addEventListener("click", () => {
+    const oldResult = Number(counter.innerHTML);
+    if (oldResult === 1) {
+      decreaseButton.disabled = false;
+    }
+    counter.innerHTML = oldResult + 1;
+  });
+
+  return quantitySelector;
+}
--- src/components/quantitySelector/quantitySelector.stories.js
+++ src/components/quantitySelector/quantitySelector.stories.js
@@ -1,30 +1,10 @@
-import { useEffect } from "@storybook/client-api";
 import "./quantitySelector.css";
-import quantitySelector from "./quantitySelector.html";
+import { createQuantitySelector } from "./quantitySelector";
 
 export default { title: "quantitySelector" };
 
 export const simpleSelector = () => {
-  useEffect(() => {
-    const decrease = document.querySelector(".decrease-btn");
-    const increase = document.querySelector(".increase-btn");
-    const resultElement = document.querySelector(".counter");
+  const quantitySelector = createQuantitySelector();
 
-    decrease.addEventListener("click", () => {
-      const oldResult = Number(resultElement.innerHTML);
-      if (oldResult === 2) {
-        decrease.disabled = true;
-      }
-      resultElement.innerHTML = oldResult - 1;
-    });
-
-    increase.addEventListener("click", () => {
-      const oldResult = Number(resultElement.innerHTML);
-      if (oldResult === 1) {
-        decrease.disabled = false;
-      }
-      resultElement.innerHTML = oldResult + 1;
-    });
-  });
   return quantitySelector;
 };
